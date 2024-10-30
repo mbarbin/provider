de06c45cda0ad4aa787224dacce9f0b8cb461f31
Mathieu Barbin <mathieu.barbin@gmail.com>
2024-10-30 20:31:02 +0100

    Reduce dependencies in testing libs

--- test/test_interfaces/int_printer.ml
+++ test/test_interfaces/int_printer.ml
@@ -20,5 +20,5 @@ let print (Provider.T { t; handler }) i =
   let module M =
     (val Provider.Handler.lookup handler ~trait:Provider_interface.Int_printer)
   in
-  print_endline (M.string_of_int t i)
+  Stdlib.print_endline (M.string_of_int t i)
 ;;
