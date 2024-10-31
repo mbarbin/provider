e514c86e632f584690c3fdef4fb520fba163c2ef
Mathieu Barbin <mathieu.barbin@gmail.com>
2024-10-31 11:26:46 +0100

    Add custom higher-kinded mini-library

---
+++ test/higher_kinded/higher_kinded.ml
@@ -0,0 +1,19 @@
+type !'a t
+type !'a hk = 'a t
+
+module type S = sig
+  type 'a t
+  type higher_kinded
+
+  val inject : 'a t -> ('a -> higher_kinded) hk
+  val project : ('a -> higher_kinded) hk -> 'a t
+end
+
+module Make (X : sig
+    type 'a t
+  end) : S with type 'a t := 'a X.t = struct
+  type higher_kinded
+
+  external inject : 'a X.t -> ('a -> higher_kinded) hk = "%identity"
+  external project : ('a -> higher_kinded) hk -> 'a X.t = "%identity"
+end
